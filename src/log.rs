7a6fab9279e68e5c13635344e53107be6db3f006
not-forest <notforest69@gmail.com>
2025-04-06 22:37:57 +0200

    Added implementation for application logger. TODO! Add additional logging to report file for future sending via USB connection to the host laptop from the drum.

---
+++ src/log.rs
@@ -0,0 +1,62 @@
+//! Custom semihosting logger.
+
+use rtt_target::rprintln;
+use log::{Log, Level, SetLoggerError};
+
+/// Semihosting debug logger for taiko drum board.
+struct TaikoLogger;
+
+const APP_LOGGER: TaikoLogger = TaikoLogger; 
+
+impl TaikoLogger {
+    /// Initializes global [`TaikoLogger`] structure for the application.
+    ///
+    /// # Debug
+    ///
+    /// While in debug build, uses Trace logging level.
+    fn init() -> Result<(), SetLoggerError> {
+        log::set_logger(&APP_LOGGER)
+            .map(|_l| {
+                rtt_target::rtt_init_print!();
+                
+                #[cfg(debug_assertions)] {
+                    log::set_max_level(log::LevelFilter::Trace);
+                } 
+                #[cfg(not(debug_assertions))] {
+                    log::set_max_level(log::LevelFilter::Info);
+                } 
+            })
+    }
+}
+
+impl Log for TaikoLogger {
+    fn enabled(&self, metadata: &log::Metadata) -> bool {
+        #[cfg(debug_assertions)] {
+            metadata.level() <= Level::Trace
+        } 
+        #[cfg(not(debug_assertions))] {
+            metadata.level() <= Level::Info
+        }
+    }
+
+    fn log(&self, record: &log::Record) {
+        if self.enabled(record.metadata()) {
+            rprintln!("{{{}}}, [{}], {}", 
+                record.target(), 
+                record.level(), 
+                record.args()
+            ); 
+        }
+    }
+
+    fn flush(&self) {}
+}
+
+/// Initializes global [`TaikoLogger`] structure for the application.
+///
+/// # Debug
+///
+/// While in debug build, uses Trace logging level.
+pub fn init() -> Result<(), SetLoggerError> {
+    TaikoLogger::init()
+}
